64fbad5618c9cc8dbe9a2e99a930a51dfca9a164
jens <jens.dahlmanns@stud.hn.de>
2018-10-16 17:58:53 +0200

    add todo

--- issues/14/tests/testconvexhull_empty.py
+++ issues/14/tests/testconvexhull_empty.py
@@ -2,6 +2,7 @@ from gamera.core import *
 init_gamera()
 from gamera.plugins.geometry import convex_hull_from_points
 
+# TODO
 img = Image((0,0), (0,4), ONEBIT)
 img.fill(1)
 p = img.contour_pavlidis()
